4701aee98eec74a910a9cc7d31e630c5ede5c56a
YaozhiweiGit <43053658+YaozhiweiGit@users.noreply.github.com>
2019-04-17 15:28:30 +0800

    ok

---
+++ fczb/fclist.py
@@ -0,0 +1,4 @@
+from requestget import geturl
+import requests
+json1='http://tvlive.fengcaizb.com/v1/fczb/config.json'
+url='http://tvlive.fengcaizb.com/channels/channelsfc'
--- requestget.py
+++ requestget.py
@@ -2,6 +2,7 @@ import requests
 def geturl(url,headers):
     r=requests.get(url,headers=headers)
     r.encoding='utf-8'
+    #r.encoding='GB2312'
     r=r.text
     #print(r)
     return r
