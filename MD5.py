39adf30d96c5eec12d6fec4bb5a65137d7e4e235
YaozhiweiGit <43053658+YaozhiweiGit@users.noreply.github.com>
2019-04-19 18:42:04 +0800

    ok

--- MD5.py
+++ MD5.py
@@ -1,6 +1,6 @@
 import hashlib
-def get_token(str):
-    m1= hashlib.md5()
-    m1.update(str.encode("utf-8"))
-    token = m1.hexdigest()
+def get_token(message):
+    md5= hashlib.md5()
+    md5.update(message.encode("utf-8"))
+    token = md5.hexdigest()
     return token
--- hljtvGFY.py
+++ hljtvGFY.py
@@ -19,19 +19,27 @@ for m in name:
     n3=x.find('" baseUrl="')+len('" baseUrl="')
     n4=x.find('"><item url')
     baseurl = x[n3:n4]
-    baseurl=baseurl+"sd.live.m3u8"
+    baseurl=baseurl+"sd/live.m3u8"
     #print(m,baseurl)
-    millis = int(round(time.time() * 1000))
-    playerVersion ="4.12.180327_RC"
-    refererurl ="http://www.hljtv.com/live/"
-    #print(baseurl)
-    md5str="192e532733724cc07a871cbcd5f47977"+playerVersion+refererurl+str(millis)+baseurl+"192e532733724cc07a871cbcd5f47977"
-    #print(md5str)
-    hash=get_token(md5str)
+    n=time.time()
+    #print(n)
+    millis = int(n * 1000)
     #print(millis)
-    #print(hash)
-    url2=drm+"?refererurl="+refererurl+"&url="+baseurl+"&hash="+hash+"&playerVersion="+playerVersion+"&time="+str(millis)
-    #http://www.hljtv.com/m2o/player/drmx.php?refererurl=http%3A%2F%2Fwww.hljtv.com%2Flive%2F&url=http%3A%2F%2Fstream1.hljtv.com%2Fhljgg%2Fsd%2Flive.m3u8&hash=b268334db19fb2a7f5452cd740faefb7&playerVersion=4.12.180327_RC&time=1554365853382
+    playerVersion ="4.12.180327_RC"
+    refererurl ="http://www.hljtv.com/"
+    #print(baseurl)
+    md5str='862DF6728D919D06E3182D5129832559'+playerVersion+refererurl+str(millis)+baseurl+'862DF6728D919D06E3182D5129832559'
+    #print(md5str))
+    hs=get_token(md5str)
+    #print(millis)
+    #print(hs)
+    url2=drm+"?refererurl="+refererurl+"&url="+baseurl+"&hash="+hs+"&playerVersion="+playerVersion+"&time="+str(millis)
+    #url2="http://www.hljtv.com/m2o/player/drmx.php?refererurl=http%3A%2F%2Fwww.hljtv.com%2Flive%2F&url=http%3A%2F%2Fstream1.hljtv.com%2Fhljgg%2Fsd%2Flive.m3u8&hash="+hs+"&playerVersion=4.12.180327_RC&time="+str(millis)
+    #data:text/html,http://stream2.hljtv.com/hljwy/sd/live.m3u8?_upt=708d2cd31555672036
+    #http://www.hljtv.com/m2o/player/drmx.php?playerVersion=4%2E12%2E180327%5FRC&refererurl=http%3A%2F%2Fwww%2Ehljtv%2Ecom%2Flive%2Ffolder423%2F&time=1555664832061&hash=a9cf43cb8e8f6a3a06f8adfdef542f59&url=http%3A%2F%2Fstream2%2Ehljtv%2Ecom%2Fhljwy%2Fsd%2Flive%2Em3u8
+    #http://stream2.hljtv.com/hljwy/sd/live.m3u8?_upt=2ca7f1351555671118
+    #http://stream2.hljtv.com/hljwy/sd.live.m3u8?_upt=3f0579421555671803
+    #print(url2)
     #print(url2)
     playurl=geturl(url2,headers)
     print(m,playurl)
