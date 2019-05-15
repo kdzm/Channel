2e0c74696e19704a0ebff7e207311eaad7242cca
YaozhiweiGit <43053658+YaozhiweiGit@users.noreply.github.com>
2019-05-15 14:05:10 +0800

    ok

--- 爱奇艺/爱奇艺m3u8无加密.py
+++ 爱奇艺/爱奇艺m3u8无加密.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 from DPL频道列表模板 import lists
 file=open("C:\\Users\\Administrator\\PycharmProjects\\频道破解\\爱奇艺\\iqiyi.txt","r+",encoding='utf-8')
 f=file.read()
@@ -14,3 +15,6 @@ for u in url:
 file.close()
 os.system('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Daum\\PotPlayer 64 bit.exe')
 os.system('C:\\Users\\Administrator\\PycharmProjects\\频道破解\\爱奇艺\\iqiyi2.dpl')
+s=u'\u6cb3\u5357\u65b0\u519c\u6751\u9891\u9053'
+#a=s.decode('raw_unicode_escape')
+print(s)
